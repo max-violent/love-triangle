4b3b269077218e98ddf860956f53c4e2501e8fa4
max-violent <max.violent@gmail.com>
2018-09-20 15:41:54 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var count=0;
+	preferences.unshift(0);
+	for (var i=1; i<preferences.length; i++) {
+		if ((preferences[i]!=0)&(preferences[i]!=i)) {
+		if (i===preferences[preferences[preferences[i]]]) {
+			count++;
+			preferences[i]=preferences[preferences[i]]=preferences[preferences[preferences[i]]]=0;
+}
+}
+}
+	return count;
 };
